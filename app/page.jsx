2fea7d83c5a68b37549b14169e4efbca430c5a24
ken13-spect <00120620@uca.edu.sv>
2025-01-12 12:59:41 -0600

    :lipstick: Add home page styles

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,21 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-   <div className="container bg-slate-500 p-7 flex flex-col items-center h-svh mx-auto rounded-lg" >
-   <h1 className="font-semibold text-7xl text-center uppercase">Welcome to practice app user</h1>  
-   <p>This page is a one </p>
-   <button className="rounded-l">User</button>
-   </div>
+    <div className="container bg-gradient-to-br from-[#6025F5] to-[#FF5555]
+    p-7 flex flex-col items-center h-svh mx-auto rounded-lg">
+      <h1 className="font-semibold text-7xl text-center uppercase">
+        Welcome to practice Next user App
+      </h1>
+      <p className="font-semibold text-2xl text-center my-2">
+        This is a test project to explore the NextJs environment in building
+        user interfaces.{" "}
+      </p>
+      <p className="font-normal text-xl text-center my-3">
+        In the next button you will find a panel with multiple user cards that
+        are being consumed from a practice ApiRest.
+      </p>
+      <button className="rounded-xl bg-white text-pink-500 px-5 py-3 text-base font-medium transition duration-200 hover:shadow-lg hover:shadow-indigo-300">
+        Users
+      </button>
+    </div>
   );
 }
